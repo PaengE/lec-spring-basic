ff93b9530aab885ad9cdec03664b8c1f66c90b26
PaengE <ckddn2820@gmail.com>
2021-07-27 17:35:32 +0900

    Convert to Spring

--- src/main/java/hello/springbasic/OrderApp.java
+++ src/main/java/hello/springbasic/OrderApp.java
@@ -5,14 +5,19 @@ import hello.springbasic.member.Member;
 import hello.springbasic.member.MemberService;
 import hello.springbasic.order.Order;
 import hello.springbasic.order.OrderService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
     public static void main(String[] args) {
 
-        AppConfig appConfig = new AppConfig();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+//        OrderService orderService = appConfig.orderService();
 
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
