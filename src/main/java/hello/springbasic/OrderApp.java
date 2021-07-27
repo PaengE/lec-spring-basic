7fc6c52e5a63431c6b7ec71b6d26c2728b03f95f
PaengE <ckddn2820@gmail.com>
2021-07-27 16:33:46 +0900

    Fix 구현 클래스 의존 관계 제거(DIP 준수)

--- src/main/java/hello/springbasic/OrderApp.java
+++ src/main/java/hello/springbasic/OrderApp.java
@@ -3,15 +3,16 @@ package hello.springbasic;
 import hello.springbasic.member.Grade;
 import hello.springbasic.member.Member;
 import hello.springbasic.member.MemberService;
-import hello.springbasic.member.MemberServiceImpl;
 import hello.springbasic.order.Order;
 import hello.springbasic.order.OrderService;
-import hello.springbasic.order.OrderServiceImpl;
 
 public class OrderApp {
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
-        OrderService orderService = new OrderServiceImpl();
+
+        AppConfig appConfig = new AppConfig();
+
+        MemberService memberService = appConfig.memberService();
+        OrderService orderService = appConfig.orderService();
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
