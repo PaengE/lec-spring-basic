ab533b873ea39f37825f03dec48a484c49242226
PaengE <ckddn2820@gmail.com>
2021-07-31 17:13:13 +0900

    Test: Qualifier test

--- src/main/java/hello/springbasic/order/OrderServiceImpl.java
+++ src/main/java/hello/springbasic/order/OrderServiceImpl.java
@@ -5,14 +5,19 @@ import hello.springbasic.member.Member;
 import hello.springbasic.member.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
+
+    public OrderServiceImpl(MemberRepository memberRepository, @Qualifier("rDiscountPolicy") DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
